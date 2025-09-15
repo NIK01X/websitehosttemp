4958cfc40858d971644dc3e1e5d475f6a0d9e2a0
Aristo Apps <aristoappsdev@gmail.com>
2025-09-15 18:23:13 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? "/websitehosttemp/" : "/",
+  base: "/websitehosttemp/",
   plugins: [react(), tailwindcss()],
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
